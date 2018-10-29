6439de077fcbef9c3d210f152c3693b72ab6cd16
Thierry STOLLSTEINER <stollsteiner.thierry@hotmail.fr>
2018-10-29 13:50:33 +0100

    création d'une première vue home.html.twig et d'une route /home avec symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder9
+ * Date: 29/10/18
+ * Time: 11:23
+ */
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @return Response
+     * @throws \Exception
+     * @route("/home")
+     */
+    public function index()
+    {
+
+        return $this->render('home.html.twig');
+
+    }
+
+}
